c8dd78281aaf97c8b6b8ad44453120d0bae59efb
WeRockStar <kotchaphan.m@gmail.com>
2019-12-07 13:12:24 +0700

    Initial commit

---
+++ PracticeSwift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PracticeSwift
+//
+//  Created by Kotchaphan Muangsan on 7/12/2019 AD.
+//  Copyright © 2019 werockstar. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
