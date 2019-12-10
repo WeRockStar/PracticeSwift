daa068b7875b2f7cbc66fed2478b4e6ee3a71a88
WeRockStar <kotchaphan.m@gmail.com>
2019-12-10 23:48:15 +0700

    Add AppCoordinator

---
+++ PracticeSwift/AppCoordinator.swift
@@ -0,0 +1,20 @@
+//
+// Created by Kotchaphan Muangsan on 10/12/2019 AD.
+// Copyright (c) 2019 werockstar. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinator {
+
+    private let navigation: UINavigationController
+
+    init(navigation: UINavigationController) {
+        self.navigation = navigation
+    }
+
+    func start() {
+
+    }
+
+}
